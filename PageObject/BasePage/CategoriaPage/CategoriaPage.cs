41ef7dd92f2a6763203a9918847291449cb33237
emily.rojas <emilyrangelrojas@gmail.com>
2026-06-22 16:11:13 -0600

    pruebacategoria

---
+++ PageObject/BasePage/CategoriaPage/CategoriaPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using Tool_Shop.PageObject;
+
+namespace Tool_shop.PageObject.CategoriaPage
+{
+    public class CategoriaPage : BasePage
+    {
+        private readonly string homeUrl = "https://practicesoftwaretesting.com/";
+
+        private By btnInicioMenu = By.CssSelector("a[data-test='nav-home']");
+
+      
+        private By chkCategoria = By.XPath("//label[contains(text(),'Hammer')] | //input[contains(@data-test,'category')]/following-sibling::label[contains(text(),'Hammer')]");
+
+        private By lblPrimerProductoFiltrado = By.CssSelector("[data-test='product-name']");
+
+        public CategoriaPage(IWebDriver driver) : base(driver) { }
+
+ 
+        public void GoTo()
+        {
+            Driver.Navigate().GoToUrl(homeUrl);
+        }
+
+        public void IrAlInicio()
+        {
+          
+            WaitForElementToBeClickable(btnInicioMenu).Click();
+
+
+            System.Threading.Thread.Sleep(1500);
+        }
+
+        public void SeleccionarCategoriaHammer()
+        {
+           
+            IrAlInicio();
+
+          
+            var checkboxLabel = WaitForElementToBeClickable(chkCategoria);
+            checkboxLabel.Click();
+
+          
+            System.Threading.Thread.Sleep(2500);
+        }
+
+ 
+        public string ObtenerNombreDelProductoFiltrado()
+        {
+            return WaitForElement(lblPrimerProductoFiltrado).Text;
+        }
+    }
+}
---
+++ Test/CategoriaTest/CategoriaTest.cs
@@ -0,0 +1,30 @@
+﻿using Tool_shop.PageObject.CategoriaPage;
+using Tool_shop.PageObject.InicioSesionPage;
+using Tool_Shop.Tests;
+
+namespace Tool_shop.Test.CategoriaTest
+{
+    [TestFixture]
+    public class CategoriaTest : BaseTest
+    {
+        public CategoriaTest() : base("Filtro Categoria") { }
+
+        [Test]
+        public void FiltrarPorCategoria()
+        {
+            var loginPage = new InicioSesionPage(Driver);
+            var categoriaPage = new CategoriaPage(Driver);
+
+         
+            loginPage.GoTo();
+            loginPage.Login("user1515@gmail.com", "User1515*");
+
+            categoriaPage.SeleccionarCategoriaHammer();
+
+            string productoVisible = categoriaPage.ObtenerNombreDelProductoFiltrado();
+
+            Assert.That(productoVisible, Does.Contain("Hammer").IgnoreCase.Or.Contains("Mallet").IgnoreCase,
+                $"El filtro falló o se mostró un producto incorrecto: {productoVisible}");
+        }
+    }
+}
