98f815fcc1c36cfb6db55578ba8a5e14f436057d
emily.rojas <emilyrangelrojas@gmail.com>
2026-06-22 10:42:26 -0600

    pruebabusqueda

--- Test/BusquedaTest/BusquedaTest.cs
+++ Test/BusquedaTest/BusquedaTest.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Tool_shop.PageObject.BusquedaPage;
+using Tool_Shop.Tests;
 
-namespace Tool_Shop.Test.BusquedaTest
+namespace Tool_shop.Test.BusquedaTest
 {
-    internal class BusquedaTest
+    [TestFixture]
+    public class BusquedaTest : BaseTest
     {
+        public BusquedaTest() : base("Busqueda") { }
+
+        [Test]
+        public void BuscarProducto_DeberiaMostrarloEnCuadricula()
+        {
+            var busquedaPage = new BusquedaPage(Driver);
+
+          
+            busquedaPage.GoTo();
+
+           
+            busquedaPage.RealizarBusqueda("Slip");
+
+            
+            string productoObtenido = busquedaPage.ObtenerNombreDelPrimerProducto();
+
+           
+            Assert.That(productoObtenido, Does.Contain("Slip Joint Pliers"),
+                $"La búsqueda falló. Se encontró el producto: {productoObtenido}");
+        }
     }
 }
